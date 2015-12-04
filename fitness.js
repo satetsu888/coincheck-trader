051510a6bda3658ebab51bdb9a852419790af204
shotaro.akiyama <satetsu888@gmail.com>
2015-12-04 23:45:18 +0900

    Add calc property

--- fitness.js
+++ fitness.js
@@ -1,10 +1,22 @@
 var config = require('./config.js');
 
-module.exports = {
-    calcFitness: function(entity){
-        var fitness = 0;
+module.exports = (function(){
+
+    var calcFitness = function(entity){
+        var stat = {
+            current_yen: 40000,
+            current_btc: 1,
+        };
+        var calcProperty = function(stat, last_rate){
+            return stat.current_yen + stat.current_btc * last_rate;
+        };
+
         // TODO
 
-        return fitness;
-    }
-};
+        return calcProperty(stat, 40000);
+    };
+
+    return {
+        calcFitness: calcFitness
+    };
+})();
