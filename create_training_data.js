cc4c7aa18d5a57845397d009da4cb1118559925c
shotaro.akiyama <satetsu888@gmail.com>
2015-12-08 23:40:20 +0900

    sort train data by date

--- create_training_data.js
+++ create_training_data.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var mysql      = require('mysql');
 var connection = mysql.createConnection( config.mysql );
 
-connection.query('select * from trades order by id desc limit 100000', function(error,results,fields){
+connection.query('select * from trades order by id limit 1000', function(error,results,fields){
     //console.log(error);
     //console.log(results);
     fs.writeFile('train.json', JSON.stringify(results), function(){
