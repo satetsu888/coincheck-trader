6fcd15279a3bf62a857846d4c0989e1edb17b73a
shotaro.akiyama <satetsu888@gmail.com>
2015-12-22 22:38:20 +0900

    Add event loop genetic script

---
+++ genetic-node.js
@@ -0,0 +1,96 @@
+global.base_dir = __dirname;
+var co = require('co');
+
+var Fitness = require(base_dir + '/fitness.js');
+
+
+var userData = {
+    tradesLength: 102,
+    orderSize: 1,
+    fitness: new Fitness('train.json'),
+};
+
+function getRandomSolution(callback) {
+    var length = userData.tradesLength;
+    var orderSize = userData.orderSize;
+
+    var createRand = function(orderSize){
+        var rand = Math.random() * orderSize * 2 - orderSize;
+        return Math.floor(rand * 100000) / 100000;
+    };
+
+    var seed = new Array(length);
+    for(var i = 0; i < length; i++){
+        seed[i] = createRand(orderSize);
+    }
+    callback(seed);
+};
+
+function fitness(solution, callback) {
+    co(function* (){
+        //console.log(solution);
+        var result = yield userData.fitness.calcAssetsAsync(solution);
+        console.log(result);
+        callback(result);
+    }).catch(function(err){
+        console.log(err);
+    });
+};
+
+function mutate(solution, callback){
+    var orderSize = userData.orderSize;
+
+    function mutateAt(entity, index) {
+        var rand = Math.random() * orderSize * 2 - orderSize;
+        entity[index] = Math.floor(rand * 100000) / 100000;
+        return entity;
+    }
+
+    for(var i=0;i<solution.length / 2 ; i++){
+        solution = mutateAt(solution, Math.floor(Math.random() * userData.tradesLength));
+    }
+    callback(solution);
+};
+
+function crossover(father, mother, callback){
+    var length = userData.tradesLength;
+    var son      = new Array(length);
+
+    for(var i = 0; i < length; i++){
+        var ca = Math.floor(Math.random()*length);
+        var cb = Math.floor(Math.random()*length);		
+        if (ca > cb) {
+            son[i] = father[i];
+        } else {
+            son[i] = mother[i];
+        }
+    }
+
+    callback(son);
+};
+
+function stopCriteria() {
+    console.log("generation: " + this.generation);
+      return (this.generation == 10);
+}
+
+
+var  options = {
+    getRandomSolution : getRandomSolution,
+    popSize : 40,
+    stopCriteria : stopCriteria,
+    fitness : fitness,
+    minimize : false,
+    mutateProbability : 0.2,
+    mutate : mutate,
+    crossoverProbability : 0.4,
+    crossover : crossover,
+}
+
+var Task = require('genetic').Task
+  , taskInstance = new Task(options);
+
+taskInstance.run(function (stats) {
+    console.log('results');
+    console.log(stats);
+});
